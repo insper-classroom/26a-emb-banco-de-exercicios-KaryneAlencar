c1fd532fc3e9e723c3365bc4737f6322611931d0
karyne <karyneam@al.insper.edu.br>
2026-03-20 19:30:28 -0300

    c

--- pisca-led-tempo-botao/main.c
+++ pisca-led-tempo-botao/main.c
@@ -11,12 +11,69 @@
 #include "hardware/timer.h"
 #include "hardware/irq.h"
 
+const int BTN_PIN = 28;
+const int LED_PIN = 5;
+
+volatile bool pisca = false;
+volatile bool btn_press = false;
+volatile uint32_t t_inicial = 0;
+volatile uint32_t t_pressionado = 500;
+volatile bool atualiza = false;
+
+void btn_callback(uint gpio, uint32_t events) {
+    if(events == 0x4){
+        btn_press = true;
+        t_inicial = to_ms_since_boot(get_absolute_time());
+    }
+    if(events == 0x8){
+        uint32_t t_final = 0;
+        t_final = to_ms_since_boot(get_absolute_time());
+        t_pressionado = t_final - t_inicial;
+        btn_press = false;
+        atualiza = true;
+    }
+}
+
+bool timer_callback(repeating_timer_t *rt) {
+    if(!btn_press){
+        pisca = true;
+
+    }
+    return true;
+}
+
 
 int main() {
     stdio_init_all();
 
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 0);
+    
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
+    gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
+    repeating_timer_t timer;
+    add_repeating_timer_ms(t_pressionado, timer_callback, NULL, &timer);
+
+    bool led_estado = false;
     while (true) {
+        if(btn_press){
+            gpio_put(LED_PIN,0);
+            btn_press = false;
+        }
+        if(pisca && !btn_press){
+            led_estado = !led_estado;
+            gpio_put(LED_PIN, led_estado);
+            pisca = false;
+        }
+        if(atualiza){
+            cancel_repeating_timer(&timer);
+            add_repeating_timer_ms(t_pressionado, timer_callback, NULL, &timer);
+            atualiza = false;
+        }
 
     }
 
